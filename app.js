9a05a8051e90a291db6d81d950e3c3c1ebcf4c38
Fran Encabo <franencabo@gmail.com>
2020-06-16 12:11:31 +0200

    Primer código de JS

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Hola");
+
+
+// Forma  de hacer un aviso
+//alert("Me llamo Agustín");
+
+
+// Declaración de variables 
+var name = "Francisco";
+var lastName = "Encabo Servián";
+console.log(name);
+
+// Se pueden declarar las variables después de las llamadas,
+// ya que JS primero hace una pasada para encontrar las variables y 
+// luego ejecuta las funciones 
+
+console.log(lastName);
+
+
+// Varias formas de concatenar, la mejor la última
+console.log(name + " " + lastName);
+
+console.log(`${name} ${lastname}`);
